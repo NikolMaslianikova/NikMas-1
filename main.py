d53d5d895cedf77fbdd0fdc919f8d88175d30ca8
NikolMaslianikova <151906520+NikolMaslianikova@users.noreply.github.com>
2023-11-26 11:08:05 +0200

    Add files via upload
    Код Hello World!

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/api/v1/hello-world-<int:variant>')
+def hello_world(variant):
+    return f"Hello World! {variant}", 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
